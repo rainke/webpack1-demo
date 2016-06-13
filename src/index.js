be6952e78e14d6cfbd9b78fe1793cf3e38b36aa6
hepeng <947798918@qq.com>
2016-06-13 13:35:03 +0800

    loader test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Button from './components/Button/Button';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>hello react</h1>
-                <h2>hello react</h2>
+                <h2>hello hp</h2>
+                <h3>hello gudong</h3>
             </div>
         )
     }
 }
 let root = document.getElementById('app');
-ReactDOM.render(<App />,root);
+// ReactDOM.render(<App />,root);
+ReactDOM.render( <Button />, root );
